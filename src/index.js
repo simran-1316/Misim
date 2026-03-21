b76dc9bb99f4f7f7cf30b681c97247a192f0f802
Simran Rawat <rawatsimran2007@gmail.com>
2026-03-21 16:12:58 +0530

    Cleaned project and fixed gitignore

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('I will be a backend server')
+const express = require('express')      // express ko leke aa rahe h                                        
+
+const app = express()                   // server create ho raha
+
+app.use('/', (req, res) => {               // request 
+    res.send("I am running Express js")
+})  // call back function is known is route  handler
+app.listen(3000, () => {
+    console.log("Server Connected on the PORT Successfully");
+})
