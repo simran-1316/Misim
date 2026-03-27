782f66146eb4c2859dfc99b77976de06705dc952
Simran Rawat <rawatsimran2007@gmail.com>
2026-03-27 14:39:03 +0530

    Updated code and removed unused files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-const express = require('express')      // express ko leke aa rahe h                                        
-
-const app = express()                   // server create ho raha
-
-app.use('/', (req, res) => {               // request 
-    res.send("I am running Express js")
-})  // call back function is known is route  handler
-app.listen(3000, () => {
-    console.log("Server Connected on the PORT Successfully");
-})
+node_modules
